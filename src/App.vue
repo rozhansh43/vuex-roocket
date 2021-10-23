5ed8f785c439d3001e6842ec615e191c86a1b78f
rozhan sheikholmoluki <rozhansheikholmoluki@gmail.com>
2021-10-23 10:02:38 +0330

    store form vuex added

---
+++ src/App.vue
@@ -0,0 +1,47 @@
+<template>
+  <div id="app">
+      <h1>{{ count }}</h1>
+  </div>
+</template>
+
+<script>
+import store from 'vuex'
+
+export default {
+  name: 'app',
+  computed: {
+    count () {
+      return store.state.count
+    }
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+
+h1, h2 {
+  font-weight: normal;
+}
+
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+
+a {
+  color: #42b983;
+}
+</style>
